f23d27027bc9383150fed066dda50e08ec3c1468
Tochukwu Ozo-Osiedo <tochukwuosiedo001@gmail.com>
2025-08-04 16:01:47 +0100

    nav and footer done

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,8 +1,13 @@
+"use client";
 import Image from "next/image";
 import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
+import { RiMenu3Line } from "react-icons/ri";
+import { IoMdClose } from "react-icons/io";
 
 const Navbar = () => {
+  const [navOpen, setNavOpen] = useState(false);
+
   const navItems = [
     {
       name: "Home",
@@ -21,9 +26,10 @@ const Navbar = () => {
       url: "/signin",
     },
   ];
+
   return (
     <nav className="flex items-center justify-between py-3 px-6 shadow-md">
-      <Link href={"/"} className="flex items-center gap-2">
+      <Link href={"/"} className="flex items-center gap-2 z-50">
         <Image
           src={"/logo.png"}
           alt="logo"
@@ -31,18 +37,37 @@ const Navbar = () => {
           height={1000}
           className="w-10 h-10"
         />
-        <p className="text-2xl ">InkPress</p>
+        <p className="text-2xl max-lg:hidden">InkPress</p>
       </Link>
 
-      <div className="flex items-center gap-5">
+      {/* desktop view */}
+      <div className="flex items-center gap-8 max-lg:hidden">
         {navItems.map((item, index) => (
           <Link
+            key={index}
             href={item.url}
             className="text-lg hover:text-yellow-700 hover:underline"
           >
             {item.name}
           </Link>
         ))}
+      </div>
+
+      {/* mobile and tab view */}
+      <div className="lg:hidden z-50">
+        <button onClick={() => setNavOpen(!navOpen)} className="text-2xl">
+          {navOpen ? <IoMdClose /> : <RiMenu3Line />}
+        </button>
+      </div>
+
+      <div className={`bg-gray-50/90 h-dvh overflow-hidden w-full fixed top-0 left-0 lg:hidden ${navOpen ? "block" : "hidden"}`}>
+        <div className="flex flex-col items-center gap-16 pt-20">
+          {navItems.map((item, index) => (
+            <Link key={index} href={item.url} className="text-2xl">
+              {item.name}
+            </Link>
+          ))}
+        </div>
       </div>
     </nav>
   );
