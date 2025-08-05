4662ee61b4ebbaffa35d2f2aa0ef04c086426289
Tochukwu Ozo-Osiedo <tochukwuosiedo001@gmail.com>
2025-08-05 14:26:39 +0100

    updatwes

--- app/page.jsx
+++ app/page.jsx
@@ -1,10 +1,22 @@
-import Navbar from "@/components/Navbar";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="">
-      Hello
+    <main className="min-h-dvh bg-[url(/bg2.jpg)] bg-no-repeat bg-center bg-cover bg-fixed">
+      <section className="min-h-dvh bg-black/40">
+        <div>
+          <h1>
+            Welcome to <span>InkPress</span>
+          </h1>
+          <p>
+            A digital hub where we share book reviews. Designed for book lovers
+            to discover, review, and share their favourite reads. Whether you
+            are a casual reader or a bookworm. InkPress lets you track books you
+            have read, are reading and want to read. Write, read, and share
+            honst reviews from a pasionate comunity.
+          </p>
+        </div>
+      </section>
     </main>
   );
 }
