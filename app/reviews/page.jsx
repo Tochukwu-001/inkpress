12e09831e7f4930f7680e44cf170a0f3430fe205
Tochukwu Ozo-Osiedo <tochukwuosiedo001@gmail.com>
2025-08-18 16:04:41 +0100

    updates

--- app/reviews/page.jsx
+++ app/reviews/page.jsx
@@ -1,24 +1,41 @@
+"use client";
 import Link from "next/link";
-import React from "react";
+import React, { useEffect } from "react";
 import { MdOutlineKeyboardDoubleArrowRight } from "react-icons/md";
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "@/lib/firebase.config";
 
 const page = () => {
-    const reviews = [
-        {
-            img: "/bg1.jpg",
-            author: "John Wick",
-            book: "Den of Dragons",
-            review: "I did not enjoy den of dragons so i read love moon",
-            timestamp: "2/4/20205"
-        },
-        {
-            img: "/bg1.jpg",
-            author: "John Wick",
-            book: "Den of Dragons",
-            review: "Recusandae pariatur tempora distinctio voluptates temporibus. Ipsa numquam laudantium dolore accusantium odio magnam ducimus blanditiis autem. Molestias debitis debitis similique nostrum.",
-            timestamp: "2/4/20205"
-        },
-    ]
+  const reviews = [
+    {
+      img: "/bg1.jpg",
+      author: "John Wick",
+      book: "Den of Dragons",
+      review: "I did not enjoy den of dragons so i read love moon",
+      timestamp: "2/4/20205",
+    },
+    {
+      img: "/bg1.jpg",
+      author: "John Wick",
+      book: "Den of Dragons",
+      review:
+        "Recusandae pariatur tempora distinctio voluptates temporibus. Ipsa numquam laudantium dolore accusantium odio magnam ducimus blanditiis autem. Molestias debitis debitis similique nostrum.",
+      timestamp: "2/4/20205",
+    },
+  ];
+
+  const fetchReviews = async () => {
+    const querySnapshot = await getDocs(collection(db, "reviews"));
+    querySnapshot.forEach((doc) => {
+      // doc.data() is never undefined for query doc snapshots
+      console.log(doc.id, " => ", doc.data());
+    });
+  };
+
+  useEffect(() => {
+    fetchReviews();
+  }, []);
+
   return (
     <main className="min-h-dvh">
       <section className="grid md:grid-cols-2 lg:grid-cols-3 gap-3 md:gap-10 md:p-10 p-3">
